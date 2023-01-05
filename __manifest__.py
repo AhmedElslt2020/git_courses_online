6880f75cbab2a24d4295baef7fb442b84c97efab
AhmedElslt2020 <114757305+AhmedElslt2020@users.noreply.github.com>
2023-01-05 11:56:42 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "hms",
+
+    "data": [
+        "reports/hms_student_templates.xml",
+        "reports/hms_reports.xml",
+        "security/hms_security.xml",
+        "security/ir.model.access.csv",
+
+        "view/hms_patient.xml",
+        "view/hms_department.xml",
+        "view/hms_doctor.xml",
+        "view/hms_history.xml"
+
+    ],
+    'depends': ['crm']
+}
